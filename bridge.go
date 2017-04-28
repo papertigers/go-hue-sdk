6fd4e23db7ae6361294691e94fc467aeca0c7fa0
Mike Zeller <mike@mikezeller.net>
2017-04-28 04:02:52 -0400

    Find hue bridges and push them into an array

--- bridge.go
+++ bridge.go
@@ -1,12 +1,14 @@
 package hue
 
 import (
-	"fmt"
 	"log"
 	"net"
 	"strings"
 	"time"
 )
+
+// Hue docs say to use "IpBridge" over "hue-bridgeid"
+const _SSDPIdentifier =  "IpBridge"
 
 var _SSDPData = []string {
 	"M-SEARCH * HTTP/1.1",
@@ -26,38 +28,41 @@ func _CheckError(e error) {
 	}
 }
 
-// Discover Hue bridges via SSDP
-func Discover() []net.IP {
-	b := []net.IP{}
+// Discover Hue bridges via SSDP.
+// Returns a map of IP.String() to empty struct.
+func Discover() map[string]struct{} {
+	bridgeSet := make(map[string]struct{})
 
-	rAddr, err := net.ResolveUDPAddr("udp", "239.255.255.250:1900")
+	rAddr, err := net.ResolveUDPAddr("udp4", "239.255.255.250:1900")
 	_CheckError(err)
-	lAddr, err := net.ResolveUDPAddr("udp", "0.0.0.0:0")
+	conn, err := net.DialUDP("udp4", nil, rAddr)
 	_CheckError(err)
-	conn, err := net.DialUDP("udp", lAddr, rAddr)
+	lAddr, err := net.ListenMulticastUDP("udp4", nil, rAddr)
 	_CheckError(err)
 
 	defer conn.Close()
+	defer lAddr.Close()
 
 	// Write discovery packet to network
 	bytes :=  []byte(strings.Join(_SSDPData, "\r\n"))
 	_, err = conn.Write(bytes)
 	_CheckError(err)
 
-	// Read responses back for 5seconds
-	timeoutDuration := 5 * time.Second
-	buffer := make([]byte, 1024)
+	// Read responses back for short time period
+	timeoutDuration := 30 * time.Second
+	lAddr.SetReadDeadline(time.Now().Add(timeoutDuration))
+
 	for {
-		fmt.Println("Entering loop")
-		conn.SetReadDeadline(time.Now().Add(timeoutDuration))
-		n, addr, err := conn.ReadFromUDP(buffer)
+		buffer := make([]byte, 256)
+		n, addr, err := lAddr.ReadFromUDP(buffer)
 		if err != nil {
-			fmt.Println(err);
+			// TODO log error other than TimeOut
 			break
 		}
-		fmt.Println("Received from : ", addr)
-		fmt.Println(string(buffer[:n]))
+		if strings.Contains(string(buffer[:n]), _SSDPIdentifier) {
+			bridgeSet[addr.IP.String()] = struct{}{}
+		}
 	}
 
-	return b
+	return bridgeSet
 }
